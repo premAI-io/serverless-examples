97b0c2e131b409d638f1e6ed7f3be69f2feeba82
Anindyadeep Sannigrahi <anindyadeepsannigrahi@Anindyadeeps-MacBook-Pro.local>
2024-03-21 12:50:06 +0530

    delete runpod folder and replaced with latest deploy_runpod

--- runpod/builder/download_model.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoModelForCausalLM
-from transformers import AutoTokenizer
-
-def download_model(model_name: str):
-    AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path=model_name)
-    AutoTokenizer.from_pretrained(pretrained_model_name_or_path=model_name)
-
-if __name__ == "__main__":
-    download_model()
