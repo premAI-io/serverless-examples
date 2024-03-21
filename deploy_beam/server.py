35c03b113e1aa84b98fda395a23d8ee207158c2f
Anindyadeep Sannigrahi <anindyadeepsannigrahi@Anindyadeeps-MacBook-Pro.local>
2024-03-21 12:50:36 +0530

    run black

--- deploy_beam/server.py
+++ deploy_beam/server.py
@@ -1,4 +1,4 @@
-import json 
+import json
 import asyncio
 from queue import Empty
 from threading import Thread
@@ -7,20 +7,29 @@ from fastapi.responses import StreamingResponse
 from typing import AsyncGenerator
 from fastapi import Request
 from beam import App, Runtime, Image, Volume, RequestLatencyAutoscaler
-from transformers import AutoTokenizer, AutoModelForCausalLM, TextIteratorStreamer
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    TextIteratorStreamer,
+)
 
 # Import from constants
-from constants import NAME, CPU, GPU, PYTHON_VERSION, PACKAGES, CACHE_PATH, MODEL_ID
+from constants import (
+    NAME,
+    CPU,
+    GPU,
+    PYTHON_VERSION,
+    PACKAGES,
+    CACHE_PATH,
+    MODEL_ID,
+)
 
 app = App(
     name=NAME,
     runtime=Runtime(
         cpu=CPU,
         gpu=GPU,
-        image=Image(
-            python_version=PYTHON_VERSION,
-            python_packages=PACKAGES
-        ),
+        image=Image(python_version=PYTHON_VERSION, python_packages=PACKAGES),
     ),
     # Storage Volume for model weights
     volumes=[Volume(name="cached_models", path=CACHE_PATH)],
@@ -29,31 +38,30 @@ app = App(
 # Autoscale by request latency
 autoscaler = RequestLatencyAutoscaler(desired_latency=30, max_replicas=5)
 
+
 def load_models():
     # this is a one time function which beam uses when the server starts for the
     # very first time. After that, it loads the model from cache dir
 
     model = AutoModelForCausalLM.from_pretrained(
-        MODEL_ID,
-        cache_dir=CACHE_PATH, 
-        device_map="auto"
+        MODEL_ID, cache_dir=CACHE_PATH, device_map="auto"
     )
-    tokenizer = AutoTokenizer.from_pretrained(
-        MODEL_ID, 
-        cache_dir=CACHE_PATH
-    )
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_ID, cache_dir=CACHE_PATH)
 
     streamer = TextIteratorStreamer(tokenizer=tokenizer)
     return model, tokenizer, streamer
 
 
-# We create a simple FastAPI application with streaming responses 
+# We create a simple FastAPI application with streaming responses
+
 
 @app.asgi(workers=2, authorized=False, loader=load_models)
 def web_server(**inputs):
     app_server = FastAPI()
-    
-    async def _stream(chat_input: str, generation_kwargs: dict) -> AsyncGenerator[str, None]:
+
+    async def _stream(
+        chat_input: str, generation_kwargs: dict
+    ) -> AsyncGenerator[str, None]:
         model, tokenizer, streamer = inputs["context"]
         if isinstance(chat_input, str):
             chat_input = [{"role": "user", "content": chat_input}]
@@ -65,9 +73,7 @@ def web_server(**inputs):
         ).to("cuda")
 
         gen_kwargs = dict(
-            input_ids=input_ids,
-            streamer=streamer,
-            **generation_kwargs
+            input_ids=input_ids, streamer=streamer, **generation_kwargs
         )
 
         thread = Thread(target=model.generate, kwargs=gen_kwargs)
@@ -80,23 +86,26 @@ def web_server(**inputs):
                     yield chunk
             except Empty:
                 await asyncio.sleep(0.001)
-    
+
     @app_server.post("/stream")
     async def stream_response(request: Request):
         data = await request.json()
         prompt = data.get("messages", "")
         generation_kwargs = dict(
-            max_new_tokens = int(data.get("max_new_tokens", 512)),
-            temperature = float(data.get("temperature", 0.1)),
-            top_p = float(data.get("top_p", 0.95)),
-        )        
+            max_new_tokens=int(data.get("max_new_tokens", 512)),
+            temperature=float(data.get("temperature", 0.1)),
+            top_p=float(data.get("top_p", 0.95)),
+        )
 
         print(prompt)
         print(generation_kwargs)
 
         async def stream_gen():
-            async for chunk in _stream(chat_input=prompt, generation_kwargs=generation_kwargs):
+            async for chunk in _stream(
+                chat_input=prompt, generation_kwargs=generation_kwargs
+            ):
                 yield f"data: {json.dumps(dict(text=chunk), ensure_ascii=False)}\n\n"
+
         return StreamingResponse(stream_gen(), media_type="text/event-stream")
-    
+
     return app_server
